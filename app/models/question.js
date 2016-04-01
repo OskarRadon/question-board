1ffa9221f72631bc2f5da67d8ecd707d0498353f
Oskar Radon <oskarradonkimball@gmail.com>
2016-04-01 08:19:24 -0700

    Fix typos on models

--- app/models/question.js
+++ app/models/question.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  author: DS>attr(),
+  author: DS.attr(),
   text: DS.attr(),
   comments: DS.hasMany('comment', { async: true })
 });
