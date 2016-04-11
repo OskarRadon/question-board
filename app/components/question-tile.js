72f56133e6fc73576567d2ed55636e364b37d3eb
Oskar <oskarradonkimball@gmail.com>
2016-04-10 18:06:05 -0700

    Use computed property to display question author and text

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // questionOutput: Ember.computed('question.author', 'question.text', function() {
-  //   return this.get('question.author') + ' asked: ' + '"' + this.get('question.text') + '"';
-  // }),
+  questionOutput: Ember.computed('question.author', 'question.text', function() {
+    return this.get('question.author') + ' asked: ' + '"' + this.get('question.text') + '"';
+  }),
   actions: {
     edit(question, params) {
       this.sendAction('edit', question, params);
