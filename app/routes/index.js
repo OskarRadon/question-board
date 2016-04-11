0db26d60acaf3c48e23f42d67c46f4702d647e6e
Oskar <oskarradonkimball@gmail.com>
2016-04-10 17:23:38 -0700

    Question tile is working

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,12 +10,6 @@ export default Ember.Route.extend({
       newQuestion.save();
       this.transitionTo('index');
     },
-    // delete(comment) {
-    //   if (confirm('Are you sure you want to delete this question?')) {
-    //     comment.destroyRecord();
-    //     this.transitionTo('index');
-    //   }
-    // },
     update(question, params) {
       Object.keys(params).forEach(function(key) {
         if(params[key]!==undefined) {
@@ -27,3 +21,13 @@ export default Ember.Route.extend({
     }
   }
 });
+
+
+
+
+// delete(comment) {
+//   if (confirm('Are you sure you want to delete this question?')) {
+//     comment.destroyRecord();
+//     this.transitionTo('index');
+//   }
+// },
--- tests/integration/components/favorite-tile-test.js
+++ tests/integration/components/favorite-tile-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('display-favorites', 'Integration | Component | display favorites', {
+moduleForComponent('favorite-tile', 'Integration | Component | favorite tile', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{display-favorites}}`);
+  this.render(hbs`{{favorite-tile}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#display-favorites}}
+    {{#favorite-tile}}
       template block text
-    {{/display-favorites}}
+    {{/favorite-tile}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
